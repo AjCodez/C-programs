082413a2369ba8ee392f38e061399d92c5c3a628
Ajit Singhal <73593608+AjCodez@users.noreply.github.com>
2021-09-01 00:40:28 +0530

    Add files via upload

---
+++ Assignment 2/10.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+	float b,h,ar;
+	scanf("%f%f",&b,&h);
+	ar=0.5*b*h;
+	printf("Area of triangle = %f",ar);
+}
+
---
+++ Assignment 2/9.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+	float a,b,c;
+	scanf("%f%f",&a,&b);
+	c=180-(a+b);
+	printf("third angle of triangle = %f",c);
+}
+
