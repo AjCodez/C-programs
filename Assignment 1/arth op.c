e8f6b58e0bb6a07237828c697ecfddef9278c82f
Ajit Singhal <73593608+AjCodez@users.noreply.github.com>
2021-08-31 23:03:54 +0530

    Create arth op.c

---
+++ Assignment 1/arth op.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+	float a,b;
+	scanf("%f%f",&a,&b);
+	printf("Sum = %f\n",a+b);
+	printf("Substraction = %f\n",a-b);
+	printf("multiplication = %f\n",a*b);
+	printf("Division = %f\n",a/b);
+}
