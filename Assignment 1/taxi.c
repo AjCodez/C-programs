d39815d3e7d5c80097f8d30cd73903c7e5faac70
Ajit Singhal <73593608+AjCodez@users.noreply.github.com>
2021-08-31 23:04:37 +0530

    Add files via upload

---
+++ Assignment 1/taxi.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+    int n=3,c=0;
+    char gem[3][10]={"R","G","T"};
+    for (int j = 1; j <= n; j++) {
+        for (int i = 0; i < n-1; i++) {
+            c++;}}
+    printf("%d",c);
+    return 0;
+}
