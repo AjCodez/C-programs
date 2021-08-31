9093e626a8b6a27482a109b445d9af9f14692582
Ajit Singhal <73593608+AjCodez@users.noreply.github.com>
2021-09-01 00:39:34 +0530

    Add files via upload

---
+++ Assignment 2/4.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+	float c,f;
+	printf("%s","Enter the Celsius : ");
+	scanf("%f",&c);
+	f=(c*(9/5))+32;
+	printf("Given Celsius has %f Fahrenheit.",f);
+}
+
