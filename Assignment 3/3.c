91c95f89214534b6c1c46f0b59c92709a1584d8c
Ajit Singhal <73593608+AjCodez@users.noreply.github.com>
2021-09-04 19:40:27 +0530

    Create 3.c

---
+++ Assignment 3/3.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void fact(int a);
+void main()
+{
+int num;
+scanf("%d",&num);
+fact(num);
+}
+void fact(int N)
+{
+int i,fact=1;
+for(i=1;i<=N;i++)
+{
+fact=fact*i;
+}
+printf("%d",fact);
+}
