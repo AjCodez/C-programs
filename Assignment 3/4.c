ba1a127c96eb187b5cada3666aa7e00df012b900
Ajit Singhal <73593608+AjCodez@users.noreply.github.com>
2021-09-04 19:41:01 +0530

    Create 4.c

---
+++ Assignment 3/4.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int fact(int a);
+void main()
+{
+int num,factorial;
+scanf("%d",&num);
+factorial=fact(num);
+printf("%d",factorial);
+}
+int fact(int N)
+{
+int i,fact=1;
+for(i=1;i<=N;i++)
+{
+fact=fact*i;
+}
+return fact;
+}
